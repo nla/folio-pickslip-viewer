b153949fb2614c107f59bf69cd07c360ca8f479b
scott-yeadon <syeadon@nla.gov.au>
2025-10-23 09:43:20 +1100

    CTPL-425 make edit role uuid configurable

--- src/main/java/au/gov/nla/pickslip/config/FolioConfiguration.java
+++ src/main/java/au/gov/nla/pickslip/config/FolioConfiguration.java
@@ -17,4 +17,5 @@ public class FolioConfiguration {
   private String tenant;
   private String username;
   private String password;
+  private String RequestEditRoleUuid;
 }
--- src/main/java/au/gov/nla/pickslip/service/RequestEditService.java
+++ src/main/java/au/gov/nla/pickslip/service/RequestEditService.java
@@ -1,5 +1,6 @@
 package au.gov.nla.pickslip.service;
 
+import au.gov.nla.pickslip.config.FolioConfiguration;
 import au.gov.nla.pickslip.domain.FolioRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,8 +13,7 @@ import java.util.List;
 public class RequestEditService {
 
   private final FolioService folioService;
-
-  private static final String NLA_CIRC_RETRIEVALS_EDIT_REQUESTS_ROLE_UUID = "f985adab-ede9-4ebf-bcf3-5262f1ac6969";
+  private final FolioConfiguration folioConfiguration;
 
   public FolioRequest getRequestById(final String requestId) throws IOException {
     return folioService.folioApiGetRequestById(requestId);
@@ -21,6 +21,6 @@ public class RequestEditService {
 
   public boolean userCanEditRequest(final String username) throws IOException {
     List<String> roles = folioService.getFolioRolesForUser(username);
-    return roles.stream().anyMatch(NLA_CIRC_RETRIEVALS_EDIT_REQUESTS_ROLE_UUID::equals);
+    return roles.stream().anyMatch(folioConfiguration.getRequestEditRoleUuid()::equals);
   }
 }
